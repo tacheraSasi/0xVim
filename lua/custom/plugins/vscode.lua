20a64e03c5e146a5bd911805f6fa96ef53e3bd4f
tacherasasi <tacherasasi@gmail.com>
2025-05-29 18:24:47 +0300

    Ellie: added more plugins

--- lua/custom/plugins/vscode.lua
+++ lua/custom/plugins/vscode.lua
@@ -4,16 +4,16 @@ return {
     'Mofiqul/vscode.nvim',
     priority = 1000,
     config = function()
-      require('vscode').setup({
+      require('vscode').setup {
         -- Enable transparent background
         transparent = false,
         -- Enable italic comments
         italic_comments = true,
         -- Disable nvim-tree background color
         disable_nvimtree_bg = true,
-      })
+      }
       -- Set colorscheme after options
-      vim.cmd.colorscheme('vscode')
+      vim.cmd.colorscheme 'vscode'
     end,
   },
 
@@ -27,10 +27,10 @@ return {
       'MunifTanjim/nui.nvim',
     },
     keys = {
-      { '<C-b>', ':Neotree toggle<CR>', desc = 'Toggle file explorer', silent = true },
+      { '<C-b>', ':Neotree toggle<CR>', desc = 'Toggle file explorer', silent = false },
     },
     config = function()
-      require('neo-tree').setup({
+      require('neo-tree').setup {
         close_if_last_window = true,
         window = {
           width = 30,
@@ -48,7 +48,7 @@ return {
             hide_gitignored = false,
           },
         },
-      })
+      }
     end,
   },
 
@@ -57,7 +57,7 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require('lualine').setup({
+      require('lualine').setup {
         options = {
           theme = 'vscode',
           component_separators = { left = '', right = '' },
@@ -69,37 +69,37 @@ return {
           lualine_c = { { 'filename', path = 1 } }, -- Show relative path
           lualine_x = { 'encoding', 'fileformat', 'filetype' },
           lualine_y = { 'progress' },
-          lualine_z = { 'location' }
+          lualine_z = { 'location' },
         },
-      })
+      }
     end,
   },
 
   -- Integrated terminal (like VSCode's integrated terminal)
   {
     'akinsho/toggleterm.nvim',
-    version = "*",
+    version = '*',
     config = function()
-      require('toggleterm').setup({
+      require('toggleterm').setup {
         open_mapping = [[<C-`>]], -- Ctrl+` to toggle terminal (like VSCode)
         direction = 'float',
         float_opts = {
           border = 'curved',
         },
-      })
-      
+      }
+
       -- Terminal keybindings
       function _G.set_terminal_keymaps()
-        local opts = {buffer = 0}
+        local opts = { buffer = 0 }
         vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], opts)
         vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
         vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
         vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
         vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
       end
-      
+
       -- Auto-command to set terminal keymaps when terminal opens
-      vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+      vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
     end,
   },
 
@@ -108,12 +108,12 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
     config = function()
-      require('ibl').setup({
+      require('ibl').setup {
         indent = {
           char = '│', -- Use a continuous line for indentation
         },
         scope = { enabled = true },
-      })
+      }
     end,
   },
 
@@ -122,13 +122,13 @@ return {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
     config = function()
-      require('nvim-autopairs').setup({
+      require('nvim-autopairs').setup {
         check_ts = true, -- Use treesitter to check for pairs
         ts_config = {
           lua = { 'string' }, -- Don't add pairs in lua string treesitter nodes
           javascript = { 'template_string' }, -- Don't add pairs in javascript template_string
         },
-      })
+      }
     end,
   },
 
@@ -136,7 +136,7 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     config = function()
-      require('gitsigns').setup({
+      require('gitsigns').setup {
         signs = {
           add = { text = '│' },
           change = { text = '│' },
@@ -146,28 +146,38 @@ return {
         },
         on_attach = function(bufnr)
           local gs = package.loaded.gitsigns
-          
+
           -- Navigation between hunks
           vim.keymap.set('n', ']c', function()
-            if vim.wo.diff then return ']c' end
-            vim.schedule(function() gs.next_hunk() end)
+            if vim.wo.diff then
+              return ']c'
+            end
+            vim.schedule(function()
+              gs.next_hunk()
+            end)
             return '<Ignore>'
           end, { expr = true, buffer = bufnr })
-          
+
           vim.keymap.set('n', '[c', function()
-            if vim.wo.diff then return '[c' end
-            vim.schedule(function() gs.prev_hunk() end)
+            if vim.wo.diff then
+              return '[c'
+            end
+            vim.schedule(function()
+              gs.prev_hunk()
+            end)
             return '<Ignore>'
           end, { expr = true, buffer = bufnr })
-          
+
           -- Actions
           vim.keymap.set('n', '<leader>hs', gs.stage_hunk, { buffer = bufnr, desc = 'Stage hunk' })
           vim.keymap.set('n', '<leader>hr', gs.reset_hunk, { buffer = bufnr, desc = 'Reset hunk' })
-          vim.keymap.set('n', '<leader>hb', function() gs.blame_line({ full = true }) end, { buffer = bufnr, desc = 'Blame line' })
+          vim.keymap.set('n', '<leader>hb', function()
+            gs.blame_line { full = true }
+          end, { buffer = bufnr, desc = 'Blame line' })
           vim.keymap.set('n', '<leader>hd', gs.diffthis, { buffer = bufnr, desc = 'Diff this' })
           vim.keymap.set('n', '<leader>hp', gs.preview_hunk, { buffer = bufnr, desc = 'Preview hunk' })
         end,
-      })
+      }
     end,
   },
 
@@ -178,39 +188,39 @@ return {
       'nvim-lua/plenary.nvim',
     },
     config = function()
-      require('spectre').setup({
+      require('spectre').setup {
         mapping = {
           ['toggle_line'] = {
-            map = "dd",
+            map = 'dd',
             cmd = "<cmd>lua require('spectre').toggle_line()<CR>",
-            desc = "toggle current item"
+            desc = 'toggle current item',
           },
           ['enter_file'] = {
-            map = "<cr>",
+            map = '<cr>',
             cmd = "<cmd>lua require('spectre.actions').select_entry()<CR>",
-            desc = "goto current file"
+            desc = 'goto current file',
           },
           ['send_to_qf'] = {
-            map = "<leader>q",
+            map = '<leader>q',
             cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
-            desc = "send all items to quickfix"
+            desc = 'send all items to quickfix',
           },
           ['replace_cmd'] = {
-            map = "<leader>c",
+            map = '<leader>c',
             cmd = "<cmd>lua require('spectre.actions').replace_cmd()<CR>",
-            desc = "input replace command"
+            desc = 'input replace command',
           },
           ['show_option_menu'] = {
-            map = "<leader>o",
+            map = '<leader>o',
             cmd = "<cmd>lua require('spectre').show_options()<CR>",
-            desc = "show options"
+            desc = 'show options',
           },
         },
-      })
-      
+      }
+
       -- Keybinding to open search and replace
       vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").open()<CR>', {
-        desc = 'Open Spectre for search and replace'
+        desc = 'Open Spectre for search and replace',
       })
     end,
   },
@@ -220,14 +230,50 @@ return {
     'mg979/vim-visual-multi',
     branch = 'master',
   },
+  {
+    'f-person/git-blame.nvim',
+    config = function()
+      vim.g.gitblame_enabled = 1 -- Enable by default
+      vim.g.gitblame_message_template = '<author> • <date> • <summary>'
+      vim.g.gitblame_date_format = '%r' -- e.g. "3 days ago"
+      vim.g.gitblame_highlight_group = 'Comment'
+
+      -- Optional: Toggle Git blame
+      vim.keymap.set('n', '<leader>gb', '<Cmd>GitBlameToggle<CR>', { desc = 'Toggle git blame' })
+    end,
+  },
+  {
+    'NeogitOrg/neogit',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('neogit').setup {}
+      vim.keymap.set('n', '<leader>gg', '<cmd>Neogit<CR>', { desc = 'Open Neogit' })
+    end,
+  },
+  {
+    'stevearc/conform.nvim',
+    config = function()
+      require('conform').setup {
+        format_on_save = {
+          lsp_fallback = true,
+        },
+        formatters_by_ft = {
+          javascript = { 'prettier' },
+          typescript = { 'prettier' },
+          lua = { 'stylua' },
+          python = { 'black' },
+        },
+      }
+    end,
+  },
 
   -- Linting (like VSCode's linting)
   {
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
-      local lint = require('lint')
-      
+      local lint = require 'lint'
+
       -- Configure linters for different filetypes
       lint.linters_by_ft = {
         javascript = { 'eslint' },
@@ -236,7 +282,7 @@ return {
         lua = { 'luacheck' },
         markdown = { 'markdownlint' },
       }
-      
+
       -- Run linting when reading or writing a buffer, and on changes in insert and normal mode
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         callback = function()
