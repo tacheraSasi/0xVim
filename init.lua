58e75be12b4e5c7c981266b706b798dcd9ffd19e
tacherasasi <tacherasasi@gmail.com>
2025-05-30 10:27:04 +0300

    feat: enhance Neovim configuration with improved listchars and new plugins
    Updated the listchars in init.lua for better visual representation of whitespace characters. Added new plugin configurations for nvim-cmp, LuaSnip, and lspkind in plugins.lua to enhance completion and symbol representation. Included a loading statement for custom completion settings in init.lua.

--- init.lua
+++ init.lua
@@ -150,7 +150,14 @@ vim.o.splitbelow = true
 --   See `:help lua-options`
 --   and `:help lua-options-guide`
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { 
+  tab = '│ ', 
+  trail = '·', 
+  nbsp = '␣',
+  extends = '›',
+  precedes = '‹',
+  space = '·'
+}
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -53,4 +53,7 @@ vim.opt.smartcase = true -- Override ignorecase if search pattern contains upper
 vim.opt.completeopt = 'menuone,noselect' -- Better completion experience
 vim.opt.wildmode = 'longest:full,full' -- Better command-line completion
 vim.opt.wildmenu = true -- Enable wildmenu
-vim.opt.wildignore = '*.o,*.obj,*.dylib,*.bin,*.dll,*.so,*.pyc,*.jpg,*.png,*.gif,*.zip,*.tar.gz,*.tar.bz2,*.tar.xz,*.tar' -- Ignore these files in wildmenu 
+vim.opt.wildignore = '*.o,*.obj,*.dylib,*.bin,*.dll,*.so,*.pyc,*.jpg,*.png,*.gif,*.zip,*.tar.gz,*.tar.bz2,*.tar.xz,*.tar' -- Ignore these files in wildmenu
+
+-- Load completion configuration
+require('custom.cmp') 
